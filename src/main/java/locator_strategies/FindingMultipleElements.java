1cb184b738d03a7d2890bc08f8f9766efaa13a74
sagare-04 <pratikshasagare04@gmail.com>
2025-11-06 15:00:49 +0000

    find elements

--- src/main/java/locator_strategies/FindingMultipleElements.java
+++ src/main/java/locator_strategies/FindingMultipleElements.java
@@ -6,13 +6,19 @@ import org.openqa.selenium.WebElement;
 import setup.Main;
 import java.util.List;
 
-public class FindingMultipleElements extends Main{
+public class FindingMultipleElements extends Main {
 
   @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+    List<WebElement> radioButton = driver.findElements(By.cssSelector("input[type='radio']"));
+    System.out.println(radioButton.size());
 
-    
+    for (WebElement radio : radioButton) {
+      String value = radio.getAttribute("value"); // ✅ must be String
+      System.out.println("Radio value: " + value);
+
+    }
     driver.quit();
   }
 

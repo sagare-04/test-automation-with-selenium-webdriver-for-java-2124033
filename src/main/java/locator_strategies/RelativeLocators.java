bf44782d6bbe4cb92e647632a2141182eec2440c
sagare-04 <pratikshasagare04@gmail.com>
2025-11-06 15:16:05 +0000

    Relative locators

--- src/main/java/locator_strategies/RelativeLocators.java
+++ src/main/java/locator_strategies/RelativeLocators.java
@@ -11,8 +11,8 @@ public class RelativeLocators extends Main {
   @SuppressWarnings("unused")
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
-
-
+    WebElement submitButton = driver.findElement(RelativeLocator.with(By.name("submit"))
+        .below(By.id("submit")));
 
     driver.quit();
   }
